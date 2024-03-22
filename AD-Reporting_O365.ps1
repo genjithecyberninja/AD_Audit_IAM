fe06febe5aee061d1d91bae3ea18ef498679424f
genjithecyberninja <45523823+genjithecyberninja@users.noreply.github.com>
2024-03-22 11:54:54 -0700

    Update AD-Reporting_O365.ps1
    Added lastSignInDateTime

--- AD-Reporting_O365.ps1
+++ AD-Reporting_O365.ps1
@@ -7,4 +7,4 @@ $date = $((Get-Date).ToString('MM-dd-yyyy_hh-mm'))
 $path = "C:"+$env:HOMEPATH +"\Desktop\"
 
 # Export all users with details
-Get-MSOLUser -maxresults 1600 | Select-Object ObjectId, DisplayName, UserPrincipalName, isLicensed, PasswordNeverExpires, PasswordLastSet, StrongPasswordRequired, ValidationStatus |Export-Csv -force -NoTypeInformation -path "$path\AD_O365_Report_$date.csv"
+Get-MSOLUser -maxresults 1600 | Select-Object ObjectId, DisplayName, UserPrincipalName, isLicensed, PasswordNeverExpires, PasswordLastSet, StrongPasswordRequired, ValidationStatus, lastSignInDateTime |Export-Csv -force -NoTypeInformation -path "$path\AD_O365_Report_$date.csv"
